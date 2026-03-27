00ddd897656dfbd7c39112cc916d10c3d741403d
chaitanyakulkarni77777 <chaitanyakulkarni77777@gmail.com>
2026-03-27 22:24:50 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/User.js";
+
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+// app.use(cors({
+//   origin: "http://localhost:3000",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true
+// }));
+
+// app.options("*", cors());
+
+
+
+
+// app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); // for form data
+
+app.use("/api/user/", UserRoutes);
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "hey it is the server of chaitanya kulkarni welcome here ",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
+
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(5000, () => console.log("Server started on port 5000"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
